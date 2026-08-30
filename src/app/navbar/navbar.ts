c2fddd94d84ed5907266309964e2b6be5343e2a6
Abdul Ali <aliabdulali0712@gmail.com>
2026-08-31 02:15:13 +0530

    Add auto hiding navbar

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  styleUrl: './navbar.css'
 })
-export class Navbar {}
+export class Navbar {
+
+  isHidden = false;
+  lastScrollPosition = 0;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+
+    const currentScrollPosition = window.scrollY;
+
+    // Always show navbar at the top
+    if (currentScrollPosition <= 10) {
+      this.isHidden = false;
+      this.lastScrollPosition = currentScrollPosition;
+      return;
+    }
+
+    // Scrolling down → hide navbar
+    if (currentScrollPosition > this.lastScrollPosition) {
+      this.isHidden = true;
+    }
+
+    // Scrolling up → show navbar
+    else if (currentScrollPosition < this.lastScrollPosition) {
+      this.isHidden = false;
+    }
+
+    this.lastScrollPosition = currentScrollPosition;
+  }
+}
